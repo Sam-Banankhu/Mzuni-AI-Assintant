d045b75eb2c21b32b50c06d47204b3a6f05aab1d
Sam Banankhu <sambanankhumhango@gmail.com>
2024-02-26 15:27:58 +0200

    fixed data extraction

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_pdf_text(pdf_docs):
     for pdf in pdf_docs:
         pdf_reader =  PdfReader(pdf)
         for page in pdf_reader.pages:
-            text =+ page.extract_text()
+            text += page.extract_text()
     return text
 
 
