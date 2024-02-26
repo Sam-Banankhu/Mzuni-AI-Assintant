f6ca2afb08749ce69dd1c47d7f1a84e84cedacdc
Sam Banankhu <sambanankhumhango@gmail.com>
2024-02-26 14:14:07 +0200

    configuring sidebar

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ def main():
     st.header("Mzuni AI Assistant :books:")
     st.text_input("Ask me what you want to know about Mzuzu University")
     
-    
-    
+    with st.sidebar:
+        st.subheader("Your Documents")
+        st.file_uploader("Please upload your documents here")
+        st.button("Process")
+        
+        
+    # with st.sidebar:
+    #     st.sidebar.title("About")
+    #     st.sidebar.markdown("This is an AI assistant for Mzuzu University")
 
 if __name__ == "__main__":
     main()
