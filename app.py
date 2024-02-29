17a0139d4e142bb6af6bb1f736021772ca740ff9
Sam Banankhu <sambanankhumhango@gmail.com>
2024-02-29 23:34:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,13 @@ from PyPDF2 import PdfReader
 import re
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain_openai.chat_models import ChatOpenAI
 from langchain_community.vectorstores import FAISS
-# from file_processing.file_preprocessing import *
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
 
 def get_pdf_text(pdf_docs):
-    """_summary_
-
-    Args:
-        pdf_docs (_type_): _pdf_file_
-
-    Returns:
-        _type_: _str_
-    """
     text = ""
     
     for pdf in pdf_docs:
@@ -57,9 +51,22 @@ def create_vectors_store(text_chunks):
         
     return vector_store
 
+def get_conversation_chains(vectors_db):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, memory=memory, retriever= vectors_db
+    )
+    return conversation_chain
+
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Mzuni AI Assistant", page_icon=":books:")
+    
+    if "conversation" not in st.session_state:
+        st.session_state["conversation"] = None
+        
     st.header("Mzuni AI Assistant :books:")
     st.text_input("Ask me what you want to know about Mzuzu University")
     
@@ -76,10 +83,14 @@ def main():
                     text_chunks = get_text_chunks(raw_text)
                     # st.write(text_chunks)text_chunks, vectorstext_chunks, vectorsxt_chunks, vectors
                     # create vectors store
-                    vectors = create_vectors_store(text_chunks=text_chunks)
+                    vector_db = create_vectors_store(text_chunks=text_chunks)
+                    
+                    st.session_state.conversation = get_conversation_chains(vector_db)
+    
                 except Exception as e:
                     st.error(e)
-        
+                    
+    # st.session_state.conversation
         
     # with st.sidebar:
     #     st.sidebar.title("About")
